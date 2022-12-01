c657130269145e118c5f3522717debbaf97d0e49
Alex Frankhouser <freesoul423@gmail.com>
2022-12-01 18:23:47 -0500

    Corrected path for index.js in htmlRoutes folder

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -3,17 +3,17 @@ const router = require('express').Router();
 
 // Gets landing page
 router.get('/', ( req, res ) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 });
 
 // Gets notes page
 router.get('/notes', ( req, res ) => {
-    res.sendFile(path.join(__dirname, '../../public/notes.html'))
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
 });
 
 // Brings you back to the landing page
 router.get('*', ( req, res ) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 });
 
 module.exports = router;
